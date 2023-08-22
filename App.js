9ca7d9b7d9685c7f998b976572469c25e96d5248
Raj Goyal <rajgoyal@Apples-MacBook-Air.local>
2023-08-22 23:33:11 +0530

    chapter 1

---
+++ App.js
@@ -0,0 +1,15 @@
+// const heading = React.createElement("h1", {"id": "heading"}, "Hello Raj from React");
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am from h1 tag"),
+    React.createElement("h2", {}, "I am from h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am from h1 tag"),
+    React.createElement("h2", {}, "I am from h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
