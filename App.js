6dcdca0c9e2079392f150b94f2ca230253c1bff6
Raj Goyal <h20211030099@hyderabad.bits-pilani.ac.in>
2023-08-28 22:21:23 +0530

    chapter 3 without css

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./logo.jpeg";
 
-// const heading = React.createElement("h1", {"id": "heading"}, "Hello Raj from React");
+//React Element
+const jsxheading = (
+  <h1 className="heading" tabIndex="5">
+    Learn React using jsx
+  </h1>
+);
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am from h1 tag"),
-    React.createElement("h2", {}, "I am from h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am from h1 tag"),
-    React.createElement("h2", {}, "I am from h2 tag"),
-  ]),
-]);
+//Functional Component
+
+const TitleComponent = () => (
+  <h1>Title for learning react through component</h1>
+);
+
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <TitleComponent />
+      {jsxheading}
+      {TitleComponent()} {/* can call TitleComponent as it is a function */}
+      <h1 className="heading">Learn React using heading component</h1>
+    </div>
+  );
+};
+
+// assignment 3:
+
+const HeaderComponent = () => {
+  return (
+    <div id="container" class="row">
+      <img src={logo} height="50px" />
+      <input type="text" placeholder="Search.." />
+      <i class="fa fa-user" />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(jsxheading);
+root.render(<HeaderComponent />);
